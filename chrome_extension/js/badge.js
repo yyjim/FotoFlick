c5a92b671344beecab3af8a3c1e5fe4abe4193bc
yenting <yenting@cardinalblue.com>
2013-11-02 21:17:21 +0800

    Display badge to show available images to flick

---
+++ chrome_extension/js/badge.js
@@ -0,0 +1,16 @@
+function updateAvailableImgCount() {
+    var imgs = document.getElementsByTagName('img');
+    var img_count = 0;
+
+    for (var i in imgs) {
+        var img = imgs[i];
+        if (img.width && img.height) {
+            if (img.width > 150 || img.height > 150) {
+                img_count++;
+            }
+        }
+    }
+    return img_count;
+}
+
+chrome.runtime.sendMessage({img_count: updateAvailableImgCount()}, function(response) {});
