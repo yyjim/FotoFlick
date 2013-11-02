53c66e282da8c73a4d3c6c1df9df818b676d7386
yenting <yenting@cardinalblue.com>
2013-11-02 12:04:38 +0800

    Create DOM and append to first place

--- chrome_extension/js/contentscript.js
+++ chrome_extension/js/contentscript.js
@@ -9,7 +9,21 @@ function fetchImage() {
         }
     }
   }
-  console.log(targetImgs);
+
+  var interact_dom = document.createElement('div');
+  interact_dom.setAttribute('class', 'style_interact_board');
+  for (var i in targetImgs) {
+    var img = targetImgs[i];
+    var new_img_dom = document.createElement('img');
+    if (img.width > img.height) {
+      new_img_dom.setAttribute('width', '300');
+    } else {
+      new_img_dom.setAttribute('height', '300');
+    }
+    new_img_dom.setAttribute('src', img.src);
+    interact_dom.appendChild(new_img_dom);
+  }
+  document.body.insertBefore(interact_dom, document.body.firstChild);
 }
 
 fetchImage();
