006236050fff412f6e86e90b9f4a086697690bbc
yenting <yenting@cardinalblue.com>
2013-11-02 11:27:05 +0800

    Collect usable images

--- chrome_extension/js/contentscript.js
+++ chrome_extension/js/contentscript.js
@@ -1,13 +1,15 @@
 function fetchImage() {
   var imgs = document.getElementsByTagName('img');
+  var targetImgs = new Array();
   for (var i in imgs) {
     var img = imgs[i];
     if (img.width && img.height) {
         if (img.width > 100 && img.height) {
-            console.log(img.src);
+            targetImgs.push(img);
         }
     }
   }
+  console.log(targetImgs);
 }
 
 fetchImage();
