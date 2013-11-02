0551f22d19bbd8bc44dff905a5cc11339ebbe847
yenting <yenting@cardinalblue.com>
2013-11-02 13:01:30 +0800

    Extract generate new dom

--- chrome_extension/js/contentscript.js
+++ chrome_extension/js/contentscript.js
@@ -11,6 +11,17 @@ function collectImages(data) {
   return targetImgs;
 }
 
+function generateNewImageDom(img) {
+  var new_img_dom = document.createElement('img');
+  if (img.width > img.height) {
+      new_img_dom.setAttribute('width', '300');
+    } else {
+      new_img_dom.setAttribute('height', '300');
+    }
+    new_img_dom.setAttribute('src', img.src);
+    return new_img_dom;
+}
+
 function iWantPhlickr() {
   var imgs = document.getElementsByTagName('img');
   var targetImgs = collectImages(imgs);
@@ -19,13 +30,7 @@ function iWantPhlickr() {
   interact_dom.setAttribute('class', 'style_interact_board');
   for (var i in targetImgs) {
     var img = targetImgs[i];
-    var new_img_dom = document.createElement('img');
-    if (img.width > img.height) {
-      new_img_dom.setAttribute('width', '300');
-    } else {
-      new_img_dom.setAttribute('height', '300');
-    }
-    new_img_dom.setAttribute('src', img.src);
+    var new_img_dom = generateNewImageDom(img);
     interact_dom.appendChild(new_img_dom);
   }
   document.body.insertBefore(interact_dom, document.body.firstChild);
